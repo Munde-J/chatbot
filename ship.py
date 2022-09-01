f61d89e19b84726b2ccdf95a714ed2d531ef06ec
Munde-J <mundeangelah@gmail.com>
2022-09-01 22:20:35 +0300

    added choices

--- ship.py
+++ ship.py
@@ -16,3 +16,18 @@ while Choice != "d":
 	print ("3. View your details")
 	print ("4. Exit\n")
 	Choice = input ("Enter your choice:  ")
+	if Choice == "a":
+		Location = input ("Where would you like to go? ")
+		print ("\nTravelling...")
+		time.sleep(2)
+		print (ShipName, "has arrived at", Location + ".")
+		time.sleep(0.5)
+	elif Choice == "b":
+		print ("\nFiring cannons...")
+		time.sleep(1)
+		print ("\nBANG!")
+		time.sleep(1.5)
+	elif Choice == "c":
+		print ("\nShip name:", ShipName)
+		print ("Capain:", Captain)
+		print ("Location:", Location)
